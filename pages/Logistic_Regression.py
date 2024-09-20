bc1d438aa32654f390392db437edccf257cfbf88
Marina Pozhidaeva <maricolada@MacBook-Pro-Marina.local>
2024-09-20 20:59:42 +0300

    First iteration of the app

--- pages/Logistic_Regression.py
+++ pages/Logistic_Regression.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from io import BytesIO 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
